d7a316b0973e669d5757f07558b5d7de0fbce6ec
nafish08 <md.nafish.sadik@gmail.com>
2022-05-29 11:24:04 +0600

    projects initial setup done and connected with mongodb

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config(); // for environment variable
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors()); // for communicating client and server
+app.use(express.json()); // for getting the body data
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qeown.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const servicesCollection = client.db('doctors_portal').collection('services');
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
